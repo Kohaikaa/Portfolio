c7b7c248bcc31ccb1b5d5a133e0fe27ade523784
Vladislav Kohai <cherry.designwork@gmail.com>
2023-02-13 19:33:12 +0300

    Promo is done

---
+++ dist/js/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector(".menu"),
+    closeMenu = document.querySelector(".menu__close");
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
