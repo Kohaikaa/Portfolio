c0167c5b2a9943ae81043ebad71f24d2c7b3e15a
Vladislav Kohai <cherry.designwork@gmail.com>
2023-02-16 17:35:12 +0300

    Counter script

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,10 +1,18 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector(".menu"),
     closeMenu = document.querySelector(".menu__close");
+
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeMenu.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
+
+const skillsCounter = document.querySelectorAll(".skills__ratings-counter"),
+    lines = document.querySelectorAll(".skills__ratings-line span");
+
+skillsCounter.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
